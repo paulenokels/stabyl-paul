def7f0379981ee22d2d24efbda1ecc7359128092
paulenokels <achemepaulenokela@gmail.com>
2026-01-09 21:55:57 +0100

    project structure setup

---
+++ interfaces/theme.d.ts
@@ -0,0 +1,17 @@
+export type Theme = {
+    bgColor: string;
+    secondaryBgColor: string;
+    textColor: string;
+    primaryColor: string;
+    secondaryColor: string;
+    errorColor: string;
+    mediumEmphasis: string;
+    borderColor: string;
+    lightBorderColor: string;
+    labelColor: string;
+    placeholderColor: string;
+    disabledBgColor: string;
+    linkColor: string;
+   
+  };
+  
---
+++ theme/theme.ts
@@ -0,0 +1,31 @@
+import { Theme } from '@/interfaces/theme';
+
+const primaryColor = '#3E3AFF';
+const secondaryColor = '#000000';
+const bgColor = '#FFFFFF';
+const secondaryBgColor = '#F5F5F5';
+const textColor = '#666161';
+const errorColor = '#ff0000';
+const mediumEmphasis = '#7A7A7A';
+const borderColor = '#818181';
+const lightBorderColor = '#AAAAAA50';
+const labelColor = '#9D99AC';
+const placeholderColor = '#AAAAAA';
+const disabledBgColor = '#d9d9d9';
+const linkColor = '#0a7ea4';
+
+export const theme: Theme = {
+  bgColor,
+  secondaryBgColor,
+  textColor,
+  primaryColor,
+  secondaryColor,
+  errorColor,
+  mediumEmphasis,
+  borderColor,
+  lightBorderColor,
+  labelColor,
+  placeholderColor,
+  disabledBgColor,
+  linkColor,
+};
